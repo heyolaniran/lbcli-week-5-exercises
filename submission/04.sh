06efb40b3ad968248771213c11f03ff8cb0355c1
heyolaniran <olaniran.abd@gmail.com>
2025-04-16 00:04:22 +0100

    fix: printing removed

--- submission/04.sh
+++ submission/04.sh
@@ -5,13 +5,11 @@ publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 
 hex=$(printf '%08x\n' 1495584032 | sed 's/^\(00\)*//'); 
 
-echo 
 
 
 
 LEHEX=$(echo $hex | tac -rs .. | echo "$(tr -d '\n')");
 
-echo $LEHEX
 
 # calculate the public key hash
 # - Convert to binary | sha256 | ripemd160
